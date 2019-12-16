c6ae6b17dc952b6a711e155d36b33e4a39801a57
Anna Kachalova <kachalova@uchi.ru>
2019-12-16 22:42:13 +0300

    fix hidePopup

--- src/components/popup.js
+++ src/components/popup.js
@@ -185,10 +185,17 @@ export default class Popup {
   }
 
   hidePopup() {
+    document.onkeydown = null;
     this._element.remove();
     this.removeElement();
   }
-
+  onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+    if (isEscKey) {
+      this.hidePopup();
+    }
+  }
+  
   showElement() {
     const bodyElement = document.querySelector(`body`);
     const visiblePopup = bodyElement.querySelector(`.film-details`);
@@ -199,17 +206,8 @@ export default class Popup {
     render(bodyElement, popupElement, RenderPosition.BEFOREEND);
     const popupButtonClose = popupElement
     .querySelector(`.film-details__close-btn`);
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-      if (isEscKey) {
-        this.hidePopup();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
-    document.addEventListener(`keydown`, onEscKeyDown);
-    popupButtonClose.addEventListener(`click`, ()=> {
-      this.hidePopup();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    });
+
+    document.onkeydown = (evt) => this.onEscKeyDown(evt);
+    popupButtonClose.addEventListener(`click`, ()=> this.hidePopup());
   }
 }
