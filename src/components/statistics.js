913659efa7599ab23e07057b50f5904a290de4f8
Anna Kachalova <kachalova@uchi.ru>
2020-01-23 19:42:03 +0300

    show statistics

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -57,13 +57,6 @@ export default class Statistics extends AbstractComponent {
   }
   getTemplate() {
     console.log('создание шаблона');
-    //return createStatistics({ rank: this._rank });
-    return `
-    <p class="statistic__rank">Your rank
-    <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    <span class="statistic__rank-label">
-    ${this._rank}
-    </span>
-  </p>`;
+    return createStatistics({ rank: this._rank });
   }
 }
--- src/controllers/statistic-controller.js
+++ src/controllers/statistic-controller.js
@@ -1,6 +1,6 @@
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { render } from '../utils/render.js';
+import { render, RenderPosition } from '../utils/render.js';
 
 import StatisticComponent from '../components/statistics.js';
 
@@ -8,11 +8,13 @@ export default class StatisticController {
   constructor(container) {
     this._container = container;
     this._statistic = null;
+    console.log(this._container);
     console.log('создание контроллера');
   }
   _render() {
     this._statistic = new StatisticComponent({ rank: 5 });
-    render(this._container, this._statistic.getElement());
+    console.log(this._statistic.getElement());
+    render(this._container, this._statistic, RenderPosition.BEFOREEND);
   }
   show(cards) {
     this._render();
