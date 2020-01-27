baa3eab09cc75edb5a19fe80ded925d04d48932c
Anna Kachalova <kachalova@uchi.ru>
2020-01-27 17:07:20 +0300

    fix statistics

--- src/controllers/statistic-controller.js
+++ src/controllers/statistic-controller.js
@@ -9,6 +9,31 @@ const countRank = cards => {
   const filmsHistoryCount = cards.filter(film => film.isHistory === true).length;
   return getRank(filmsHistoryCount);
 };
+const StatisticBar = {
+  data: {
+    backgroundColor: `#ffe800`,
+    hoverBackgroundColor: `#ffe800`,
+    anchor: `start`,
+  },
+  options: {
+    datalabel: {
+      fontSize: 25,
+      color: `#ffffff`,
+      anchor: `start`,
+      align: `start`,
+      offset: 40,
+    },
+    animationEasing: `easeInOutQuad`,
+    yAxes: {
+      barThickness: 20,
+      ticks: {
+        fontColor: `#ffffff`,
+        padding: 100,
+        fontSize: 25,
+      },
+    },
+  },
+};
 
 export default class StatisticController {
   constructor(container) {
@@ -33,22 +58,37 @@ export default class StatisticController {
     const rank = countRank(this._cards);
 
     // topGenre
-    this._allGenres = [];
+    //this._allGenres = [];
     this._allGenresNew = [];
 
     this._cardsList.forEach(card => {
-      this._allGenres = this._allGenres.concat(card.genre);
+      //this._allGenres = this._allGenres.concat(card.genre);
+      this._allGenres = this._cardsList.reduce(
+        (accum, film) => accum.concat(film.genre),
+        []
+      );
     });
 
     if (this._allGenres.length) {
-      this._allGenresNew = this._allGenres.reduce((accum, current) => {
+      //this._allGenresNew = this._allGenres.reduce((accum, current) => {
+      this._allGenres = this._allGenres.reduce((accum, current) => {
         accum[current] = (accum[current] || 0) + 1;
         return accum;
       }, {});
     }
-    this._topGenre = Object.keys(this._allGenresNew).find(
-      key => this._allGenresNew[key] === Math.max(...Object.values(this._allGenresNew))
+    const sortedGenres = Object.keys(this._allGenres).sort(
+      (a, b) => this._allGenres[b] - this._allGenres[a]
     );
+    this._topGenre = sortedGenres[0];
+    // Преобразуем массив обратно в объект
+    this._allGenres = sortedGenres.reduce(
+      (object, key) => Object.assign(object, { [key]: this._allGenres[key] }),
+      {}
+    );
+
+    /*this._topGenre = Object.keys(this._allGenresNew).find(
+      key => this._allGenresNew[key] === Math.max(...Object.values(this._allGenresNew))
+    );*/
 
     // quantityWatched
     this._quantityWatched = this._cardsList.length;
@@ -75,9 +115,10 @@ export default class StatisticController {
 
     if (oldComponent) {
       replace(this._statistic, oldComponent);
+      this.renderChart();
     } else {
       render(this._container, this._statistic, RenderPosition.BEFOREEND);
-      //this.renderChart();
+      this.renderChart();
     }
   }
   hide() {
@@ -99,16 +140,24 @@ export default class StatisticController {
     const getFilteredFilms = () => {
       switch (this._activeFilterType) {
         case `today`:
-          return this._watchedList.filter((el, i) => i < 1);
+          return this._watchedList.filter(film =>
+            moment().isSame(moment(film.watchedDate), `day`)
+          );
+
         case `week`:
-          console.log('неделя');
-          return this._watchedList.filter((el, i) => i < 7);
+          return this._watchedList.filter(
+            film => moment(film.watchedDate) > moment().subtract(1, `w`)
+          );
+
         case `month`:
-          console.log('месяц');
-          return this._watchedList.filter((el, i) => i < 5);
+          return this._watchedList.filter(
+            film => moment(film.watchedDate) > moment().subtract(1, `months`)
+          );
+
         case `year`:
-          console.log('год');
-          return this._watchedList.filter((el, i) => i < 2);
+          return this._watchedList.filter(
+            film => moment(film.watchedDate) > moment().subtract(1, `y`)
+          );
       }
       return this._watchedList;
     };
@@ -125,5 +174,62 @@ export default class StatisticController {
     this._canvas = this._statistic.getElement().querySelector(`.statistic__chart`);
     this._chart = new Chart(this._canvas, this._getChart());
   }
-  _getChart() {}
+  _getChart() {
+    const labels = Object.keys(this._allGenres);
+
+    const barData = {
+      labels,
+      datasets: [
+        {
+          data: Object.values(this._allGenres),
+          backgroundColor: StatisticBar.data.backgroundColor,
+          hoverBackgroundColor: StatisticBar.data.hoverBackgroundColor,
+          anchor: StatisticBar.data.anchor,
+        },
+      ],
+    };
+    const barOptions = {
+      plugins: {
+        datalabels: {
+          font: { size: StatisticBar.options.datalabel.fontSize },
+          color: StatisticBar.options.datalabel.color,
+          anchor: StatisticBar.options.datalabel.anchor,
+          align: StatisticBar.options.datalabel.align,
+          offset: StatisticBar.options.datalabel.offset,
+        },
+      },
+      animation: {
+        easing: StatisticBar.options.animationEasing,
+      },
+      scales: {
+        yAxes: [
+          {
+            barThickness: StatisticBar.options.yAxes.barThickness,
+            ticks: {
+              fontColor: StatisticBar.options.yAxes.ticks.fontColor,
+              padding: StatisticBar.options.yAxes.ticks.padding,
+              fontSize: StatisticBar.options.yAxes.ticks.fontSize,
+            },
+          },
+        ],
+        xAxes: [
+          {
+            ticks: {
+              display: false,
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      legend: { display: false },
+      tooltips: { enabled: false },
+    };
+
+    return {
+      plugins: [ChartDataLabels],
+      type: `horizontalBar`,
+      data: barData,
+      options: barOptions,
+    };
+  }
 }
